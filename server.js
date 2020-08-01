c5750e4151e913f60925b82596d617e183d2d255
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-01 14:54:02 -0700

    app.use() causes errors - needed to witch to (app) syntax

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ mongoose.connect("mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+require("./routes/api.js")(app);
+require("./routes/view.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
