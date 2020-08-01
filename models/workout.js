3471a212fdd683088b20d654590bcbf880df937b
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-01 14:49:06 -0700

    update models to onlu use workout model

--- models/workout.js
+++ models/workout.js
@@ -4,12 +4,32 @@ const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
   day: {
-    type: Date
+    type: Date,
+    default: Date.now()
   },
-  exercies: [
+  exercises: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise"
+      type: {
+        type: String
+      },
+      name: {
+        type: String
+      },
+      duration:{
+        type: Number
+      },
+      distance: {
+        type: Number
+      },
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets:  {
+        type: Number
+      }
     }
   ]
 });
