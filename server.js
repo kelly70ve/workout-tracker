b50c1f009b2d5a89506d72a7e30cef1786890c8c
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-07-31 23:17:28 -0700

    update server to have a dynamic port and require models

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
 
 const app = express();
 
