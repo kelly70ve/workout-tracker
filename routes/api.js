313b2ac689c386078719ed420132cd0382239dcf
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-01 21:57:30 -0700

    complete put request

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 const mongoose = require("mongoose");
 
+
 module.exports = function(app) {
   
   // GET all workouts and exercises
@@ -14,5 +15,47 @@ module.exports = function(app) {
       });
   });
 
+    // Put exercises
+    app.put("/api/workouts/:id", (req, res) => {
+      console.log(req.body)
+      let exercise = req.body;
+      let exerciseData;
+
+      if (exercise.type === 'resistance') {
+        exerciseData = {
+          type: exercise.type,
+          name: exercise.name,
+          weight: exercise.weight,
+          sets: exercise.sets,
+          reps: exercise.reps,
+          duration: exercise.duration
+        } 
+      } else if (exercise.type === 'cardio') {
+        exerciseData = {
+          type: exercise.type,
+          name: exercise.name,
+          distance: exercise.distance,
+          duration: exercise.duration
+        }
+      };
+
+      db.Workout.findOneAndUpdate(
+        {
+          _id: req.params.id
+        },
+        {
+          $push: {
+            exercises: exerciseData
+          }
+        },
+        (error, data) => {
+          if (error) {
+            res.send(error);
+          } else {
+            res.send(data);
+          }
+        }
+      );
+    });
 
 }
